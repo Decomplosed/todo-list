50c33d00f23f4190a6a25f4063780132d209c8c8
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-07 23:16:31 +0100

    created a constructor and parsed task as a value of input

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 
 export class NewTodoForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { task: "" }
+  }
 
   render() {
     return (
       <div>
         <form htmlFor="task">
-          <input type="text" placeholder="New Todo" id="task" />
+          <input type="text" placeholder="New Todo" id="task" value={this.state.task} />
         </form>
       </div>
     )
