108a84f45c57036a02cbb01544db9b1d3c119135
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-07 23:15:33 +0100

    created a basic form structure and added attributes

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 
 export class NewTodoForm extends Component {
+
   render() {
     return (
       <div>
-
+        <form htmlFor="task">
+          <input type="text" placeholder="New Todo" id="task" />
+        </form>
       </div>
     )
   }
