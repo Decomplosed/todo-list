f7a6b887be7bcbf935350b0f2a74c0f65e79c08a
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-07 23:17:12 +0100

    added onChange

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -10,7 +10,13 @@ export class NewTodoForm extends Component {
     return (
       <div>
         <form htmlFor="task">
-          <input type="text" placeholder="New Todo" id="task" value={this.state.task} />
+          <input
+            type="text"
+            placeholder="New Todo"
+            id="task"
+            value={this.state.task}
+            onChange={this.handleChange}
+          />
         </form>
       </div>
     )
