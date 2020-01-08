fcf524767e95336eb74f9474b4f5f04fcb8e7884
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-08 20:20:18 +0100

    added state with isEditing attribute

--- src/Todo.js
+++ src/Todo.js
@@ -3,6 +3,9 @@ import React, { Component } from 'react'
 class Todo extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      isEditing: false
+    }
     this.handleRemove = this.handleRemove.bind(this)
   }
 
